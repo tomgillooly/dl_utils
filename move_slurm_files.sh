2b33ac30bb9ab5f42cdd22803491cc56160d75df
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2019-07-29 16:45:55 +0200

    Correct logic to move non-running slurm files

--- move_slurm_files.sh
+++ move_slurm_files.sh
@@ -5,5 +5,6 @@ do
   dir=$(dirname $file)
   slurm_id=$(awk -F ":" '/slurm_job_id/{print $2}' $file)
   test $slurm_id == 'None' && continue
-  scontrol -dd show job $slurm_id 1>/dev/null 2>/dev/null  || mv slurm-"$slurm_id".out $dir
+  scontrol -dd show job $slurm_id 1>/dev/null 2>/dev/null || (test -e slurm-"$slurm_id".out && mv slurm-"$slurm_id".out $dir)
+  scontrol -dd show job $slurm_id 1>/dev/null 2>/dev/null && test -e slurm-"$slurm_id".out &&  cp slurm-"$slurm_id".out $dir
 done
